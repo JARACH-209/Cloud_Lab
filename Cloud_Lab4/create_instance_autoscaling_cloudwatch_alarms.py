c9a07321ad9a54d88f7cfeff9fe0c1fa6a6d8461
Achal Dixit <43511383+JARACH-209@users.noreply.github.com>
2020-09-16 00:15:43 +0530

    Update create_instance_autoscaling_cloudwatch_alarms.py

--- Cloud_Lab4/create_instance_autoscaling_cloudwatch_alarms.py
+++ Cloud_Lab4/create_instance_autoscaling_cloudwatch_alarms.py
@@ -2,19 +2,21 @@ import boto3
 from datetime import datetime
 from msvcrt import getch
 
-autoS_client = boto3.client('autoscaling')  #created an autoscaling client
-ec2 = boto3.resource('ec2')
-cloudwatch = boto3.client('cloudwatch')
+autoS_client = boto3.client('autoscaling')                                         # create an autoscaling client
+ec2 = boto3.resource('ec2')                                                        # create an ec2 resource object
+cloudwatch = boto3.client('cloudwatch')                                            # create a cloudwatch client
 
+# Adding scripts to load upon launching the EC2 instance
 with open("startup.sh", 'r') as f:
     script = f.read()
 
-keyname = "Cloud_Lab0"      #key pair name from cosole
+# #key pair name from cosole
+keyname = "Cloud_Lab0"      
 
 print("\nCreating Instance...\n")
 
 instances = ec2.create_instances(
-                ImageId='ami-07c8bc5c1ce9598c3',        #ami of the instance
+                ImageId='ami-07c8bc5c1ce9598c3',                                   # ami of the instance
                 InstanceType = 't2.micro',
                 KeyName = keyname, 
                 UserData = script, 
@@ -30,7 +32,7 @@ instance = instances[0]
 
 launch_config = autoS_client.create_launch_configuration(
                 LaunchConfigurationName = "lab4_as_launch_config",
-                ImageId='ami-07c8bc5c1ce9598c3',        #ami of the instance
+                ImageId='ami-07c8bc5c1ce9598c3',                                 #ami of the instance
                 InstanceType = 't2.micro',
                 KeyName = keyname, 
                 UserData = script, 
@@ -44,31 +46,31 @@ autos_group = autoS_client.create_auto_scaling_group(
                 AutoScalingGroupName='lab4_as_group',
                 LaunchConfigurationName='lab4_as_launch_config',
                 AvailabilityZones=['us-east-2a','us-east-2b','us-east-2c'], 
-                MaxSize=3,         #set the max count to 3 for scaling up to max 3 instances in a group
+                MaxSize=3,                                                       # set the max count to 3 for scaling up to max 3 instances in a group
                 MinSize=1   
                 )
 
 '''
-attach_inst = autoS_client.attach_instances(            #attaching the instance to autoscaling group
-                InstanceIds=[str(instance.id)],
+attach_inst = autoS_client.attach_instances(                                     # attaching the instance to autoscaling group
+                InstanceIds=[str(instance.id)],                                  # moved to lower part of code
                 AutoScalingGroupName='lab4_as_group')
 '''
 
-scale_out_policy = autoS_client.put_scaling_policy(
+scale_out_policy = autoS_client.put_scaling_policy(                              # Setting up a Scale-out policy
                 AutoScalingGroupName='lab4_as_group',
-                PolicyName='scale_out_policy_lab4',          #default is SimpleScaling in PolicyType
-                AdjustmentType='ChangeInCapacity',       #changeincapacity will add the given adjustment, ExactCapacity will adjust to the given value and PercentageChaengeInCapacity will change by percentage given     
-                ScalingAdjustment=1,                    #only 1 additional instace will be added
+                PolicyName='scale_out_policy_lab4',                              # default is SimpleScaling in PolicyType
+                AdjustmentType='ChangeInCapacity',                               # ChangeInCapacity will add the given adjustment, ExactCapacity will adjust to the given value and PercentageChaengeInCapacity will change by percentage given     
+                ScalingAdjustment=1,                                             # only 1 additional instace will be added
                 )
 
-scale_in_policy = autoS_client.put_scaling_policy(
+scale_in_policy = autoS_client.put_scaling_policy(                               # Setting up a Scale-in policy
                 AutoScalingGroupName='lab4_as_group',
-                PolicyName='scale_in_policy_lab4',          #default is SimpleScaling in PolicyType
-                AdjustmentType='ChangeInCapacity',       #changeincapacity will add the given adjustment, ExactCapacity will adjust to the given value and PercentageChaengeInCapacity will change by percentage given     
-                ScalingAdjustment= -1,                    #only removes 1 instace will be added
+                PolicyName='scale_in_policy_lab4',                               # default is SimpleScaling in PolicyType
+                AdjustmentType='ChangeInCapacity',                               # ChangeInCapacity will add the given adjustment, ExactCapacity will adjust to the given value and PercentageChaengeInCapacity will change by percentage given          
+                ScalingAdjustment= -1,                                           # only removes 1 instace will be added
                 )
 
-policy_desc = autoS_client.describe_policies(
+policy_desc = autoS_client.describe_policies(                                    # To get PolicyArn of the defined policies
                 AutoScalingGroupName='lab4_as_group',
                 PolicyNames=['scale_in_policy_lab4','scale_out_policy_lab4']
                 )
@@ -76,8 +78,8 @@ policy_desc = autoS_client.describe_policies(
 scale_in_arn = None
 scale_out_arn = None
 
-for each in policy_desc['ScalingPolicies']:          #policy_description is a dictionary of lists which have disctionaries
-    if each['PolicyName'] == 'scale_in_policy_lab4': #we need policy ARNs to set cloudwatch alarms
+for each in policy_desc['ScalingPolicies']:                                     # policy_description is a dictionary of lists which have disctionaries
+    if each['PolicyName'] == 'scale_in_policy_lab4':                            # we need policy ARNs to set cloudwatch alarms
         scale_in_arn = each['PolicyARN']
     if each['PolicyName'] == 'scale_out_policy_lab4':
         scale_out_arn = each['PolicyARN']
@@ -86,12 +88,12 @@ print("\nSetting up cloudwatch alarms...\n")
 
 scale_in_alarn = cloudwatch.put_metric_alarm(
                 AlarmName='scale_in_Alarm',
-                ComparisonOperator='LessThanThreshold',     #scale in
-                EvaluationPeriods=1,                        #no of evaluations that will occur before triggering the alarm
-                MetricName= 'NetworkIn',     # using NetworkIn not using CPUUTILIZATION because we are hosting website 
+                ComparisonOperator='LessThanThreshold',                         # scale in policy alarm
+                EvaluationPeriods=1,                                            # no of evaluations that will occur before triggering the alarm
+                MetricName= 'NetworkIn',                                        # using NetworkIn not using CPUUTILIZATION because we are hosting website therefore monitoring the network traffic
                 Namespace='AWS/EC2',
-                Period=60,      #monitoring period in seconds before trigger
-                Threshold=40.0, #scale-in when traffic is less than 40
+                Period=60,                                                      # monitoring period in seconds before trigger
+                Threshold=40.0,                                                 # scale-in when traffic is less than 40.0
                 AlarmDescription='Alarm when the website traffic is less than 40 percent',
                 Dimensions=[
                            {
@@ -105,12 +107,12 @@ scale_in_alarn = cloudwatch.put_metric_alarm(
 
 scale_out_alarm = cloudwatch.put_metric_alarm(
                 AlarmName='scale_out_Alarm',
-                ComparisonOperator='GreaterThanThreshold',     #scale out
-                EvaluationPeriods=1,                        #no of evaluations that will occur before triggering the alarm
-                MetricName= 'NetworkIn',     # using NetworkIn not using CPUUTILIZATION because we are hosting website
+                ComparisonOperator='GreaterThanThreshold',                      # scale out policy alarm
+                EvaluationPeriods=1,                                            # no of evaluations that will occur before triggering the alarm
+                MetricName= 'NetworkIn',                                        # using NetworkIn not using CPUUTILIZATION because we are hosting website
                 Namespace='AWS/EC2',
-                Period=60,      #monitoring period in seconds before trigger
-                Threshold=85.0, #scale-out when traffic is more than 85 percent
+                Period=60,                                                      # monitoring period in seconds before trigger
+                Threshold=85.0,                                                 # scale-out when traffic is more than 85 percent
                 AlarmDescription='Alarm when the website traffic is more than 85 percent',
                 Dimensions=[
                             {
@@ -126,11 +128,11 @@ print("\nWaiting for instace to be in running state to attach with Autoscaling G
 instance.wait_until_running()
 instance.load()
 
-attach_inst = autoS_client.attach_instances(
+attach_inst = autoS_client.attach_instances(                                    # Attaching out instance
                 InstanceIds=[str(instance.id)],
                 AutoScalingGroupName='lab4_as_group')
 
-print("Public DNS (IPv4): " + instance.public_dns_name)
+print("Public DNS (IPv4): " + instance.public_dns_name)                         # Loggin the details in a text file
 print("-----------------------------------\n")
 with open("Instance_DNS.txt","a") as out:
     dt = datetime.now()
