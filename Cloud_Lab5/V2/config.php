8e054e2f7c4f050b0a907d46a874f492a94d41e4
Achal Dixit <43511383+JARACH-209@users.noreply.github.com>
2020-10-04 21:43:37 +0530

    Update config.php

--- Cloud_Lab5/V2/config.php
+++ Cloud_Lab5/V2/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'lab5.coddw0p8k4sn.us-east-2.rds.amazonaws.com');
+define('DB_SERVER', 'ENDPOINT');
 define('DB_USERNAME', 'boto3_user');
-define('DB_PASSWORD', 'boto3_password');
+define('DB_PASSWORD', 'PASSWORD');
 define('DB_NAME', 'lab5_portal');
  
 /* Attempt to connect to MySQL database */
